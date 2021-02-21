c1db272f952161915978a7f68d2593b0603de70d
Jacob Nicoll <jdnicoll1@gmail.com>
2021-02-21 14:39:53 -0600

    Login Auth Fields

--- src/components/LoginScreen.js
+++ src/components/LoginScreen.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
-import { Button, View, Text, ImageBackground } from "react-native";
-import LoginScreenList from "./LoginScreenList";
+import { Button, View, Text, ImageBackground, TextInput } from "react-native";
+
+// next two imports for email and password icons
+import FontAwesome from "react-native-vector-icons/FontAwesome";
+import Feather from "react-native-vector-icons/Feather";
 
 export default function LoginScreen({ navigation }) {
   return (
@@ -10,10 +13,39 @@ export default function LoginScreen({ navigation }) {
         style={styles.image}
       />
       <View style={styles.header}>
-        <Text>Sign In</Text>
+        <Text style={styles.text_header}>Sign In</Text>
+        <Text>*Swipe from far left of screen to get menu</Text>
       </View>
       <View style={styles.footer}>
-        <Text>Swipe from far right of screen to get menu</Text>
+        <Text style={styles.text_footer}>Email</Text>
+        <View style={styles.action}>
+          <FontAwesome // email icon and text field
+            name="user-o"
+            color="#05375a"
+            size={20}
+          />
+          <TextInput
+            placeholder="E-mail"
+            style={styles.textInput}
+            autoCapitalize="none"
+          />
+          <Feather name="check-circle" color="green" size={20} />
+        </View>
+        <Text style={[styles.text_footer, { marginTop: 35 }]}>Password</Text>
+        <View style={styles.action}>
+          <FontAwesome // email icon and text field
+            name="lock"
+            color="#05375a"
+            size={20}
+          />
+          <TextInput
+            placeholder="Password"
+            secureTextEntry={true}
+            style={styles.textInput}
+            autoCapitalize="none"
+          />
+          <Feather name="eye-off" color="grey" size={20} />
+        </View>
       </View>
     </View>
   );
