e10863fc2b43a3d1bdf847df2c49f1b995c369df
rosspod612 <rosspod612@tamu.edu>
2021-03-03 14:10:10 -0600

    got database stuff working

--- src/components/SignUpScreen.js
+++ src/components/SignUpScreen.js
@@ -9,6 +9,7 @@ import {
   TouchableOpacity,
   StatusBar,
   Image,
+  ActivityIndicator,
 } from "react-native";
 
 // next two imports for email and password icons
@@ -17,11 +18,70 @@ import Feather from "react-native-vector-icons/Feather";
 import * as Animatable from "react-native-animatable";
 import { Video, AVPlaybackStatus } from "expo-av";
 
-export default function LoginScreen({ navigation }) {
-  const BackArrow = require("../assets/images/back_arrow.png");
-  return (
-    <View style={styles.container}>
-      {/* <Video
+import firebase from '../database/firebaseDb';
+
+class SignUpScreen extends Component {
+    constructor() {
+        super();
+        this.dbRef = firebase.firestore().collection('users');
+        this.state = {
+            email: '',
+            password: '',
+            confirmpassword: '',
+            isLoading: false
+        };
+    }
+    inputValueUpdate = (val, prop) => {
+        const state = this.state;
+        state[prop] = val;
+        this.setState(state);
+    }
+
+    storeUser() {
+        if (this.state.name === '') {
+            alert('Fill at least your name!')
+        }
+        if (this.state.password != this.state.confirmpassword) {
+            alert('passwords do not match')
+        }
+        else {
+            alert('created account')
+            this.setState({
+                isLoading: true,
+            });
+            this.dbRef.add({
+                email: this.state.email,
+                password: this.state.password,
+            }).then((res) => {
+                this.setState({
+                    email: '',
+                    password: '',
+                    confirmpassword: '',
+                    isLoading: false,
+                });
+                this.props.navigation.navigate("Home")
+            })
+                .catch((err) => {
+                    console.error("Error found: ", err);
+                    this.setState({
+                        isLoading: false,
+                    });
+                });
+        }
+    }
+    render() {
+        const BackArrow = require("../assets/images/back_arrow.png");
+        if (this.state.isLoading) {
+            return (
+                <View style={styles.preloader}>
+                    <ActivityIndicator size="large" color="#9E9E9E" />
+                </View>
+            )
+        }
+
+        return (
+            <View style={styles.container}>
+                {/* <Video
         rate={1}
         shouldPlay
         isLooping
@@ -30,95 +90,104 @@ export default function LoginScreen({ navigation }) {
         style={styles.video}
         resizeMode="cover"
       /> */}
-      <ImageBackground // when background is decided
-        source={require("../assets/images/homescreen_background.png")}
-        style={styles.image}
-      />
-      <StatusBar barStyle="light-content" />
-      <View style={styles.header}>
-        <Text style={styles.text_header}>Sign Up</Text>
-      </View>
+                <ImageBackground // when background is decided
+                    source={require("../assets/images/homescreen_background.png")}
+                    style={styles.image}
+                />
+                <StatusBar barStyle="light-content" />
+                <View style={styles.header}>
+                    <Text style={styles.text_header}>Sign Up</Text>
+                </View>
 
-      <Animatable.View style={styles.footer} animation="fadeInUpBig">
-        <Text style={styles.text_footer}>E-mail</Text>
-        <View style={styles.action}>
-          <FontAwesome // email icon and text field
-            name="user-o"
-            color="#05375a"
-            size={20}
-          />
-          <TextInput
-            placeholder="E-mail"
-            style={styles.textInput}
-            autoCapitalize="none"
-          />
-          {/* <Feather name="check-circle" color="green" size={20} /> */}
-        </View>
-        <Text style={[styles.text_footer, { marginTop: 35 }]}>Password</Text>
-        <View style={styles.action}>
-          <FontAwesome // email icon and text field
-            name="lock"
-            color="#05375a"
-            size={20}
-          />
-          <TextInput
-            placeholder="Password"
-            secureTextEntry={true}
-            style={styles.textInput}
-            autoCapitalize="none"
-          />
-          {/* <Feather name="eye-off" color="grey" size={20} /> */}
-        </View>
-        <Text style={[styles.text_footer, { marginTop: 35 }]}>
-          Confirm Password
-        </Text>
-        <View style={styles.action}>
-          <FontAwesome // email icon and text field
-            name="lock"
-            color="#05375a"
-            size={20}
-          />
-          <TextInput
-            placeholder="Confirm Password"
-            secureTextEntry={true}
-            style={styles.textInput}
-            autoCapitalize="none"
-          />
-          {/* <Feather name="eye-off" color="grey" size={20} /> */}
-        </View>
-        <View style={styles.button}>
-          <TouchableOpacity
-            onPress={() => navigation.navigate("Home")}
-            style={[
-              styles.signIn,
-              {
-                borderColor: "#05375a",
-                borderWidth: 1,
-                marginTop: 15,
-                backgroundColor: "#05375a",
-              },
-            ]}
-          >
-            <Text style={[styles.textSign, { color: "#fff" }]}>Sign Up</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            onPress={() => navigation.navigate("Log In")}
-            style={[
-              styles.signIn,
-              {
-                borderColor: "#05375a",
-                borderWidth: 1,
-                marginTop: 15,
-              },
-            ]}
-          >
-            <Image style={styles.icon} source={BackArrow} />
-          </TouchableOpacity>
-        </View>
-      </Animatable.View>
-    </View>
-  );
+                <Animatable.View style={styles.footer} animation="fadeInUpBig">
+                    <Text style={styles.text_footer}>E-mail</Text>
+                    <View style={styles.action}>
+                        <FontAwesome // email icon and text field
+                            name="user-o"
+                            color="#05375a"
+                            size={20}
+                        />
+                        <TextInput
+                            placeholder="E-mail"
+                            style={styles.textInput}
+                            value={this.state.email}
+                            onChangeText={(val) => this.inputValueUpdate(val, 'email')}
+                            autoCapitalize="none"
+                        />
+                        {/* <Feather name="check-circle" color="green" size={20} /> */}
+                    </View>
+                    <Text style={[styles.text_footer, { marginTop: 35 }]}>Password</Text>
+                    <View style={styles.action}>
+                        <FontAwesome // email icon and text field
+                            name="lock"
+                            color="#05375a"
+                            size={20}
+                        />
+                        <TextInput
+                            placeholder="Password"
+                            secureTextEntry={true}
+                            style={styles.textInput}
+                            value={this.state.password}
+                            onChangeText={(val) => this.inputValueUpdate(val, 'password')}
+                            autoCapitalize="none"
+                        />
+                        {/* <Feather name="eye-off" color="grey" size={20} /> */}
+                    </View>
+                    <Text style={[styles.text_footer, { marginTop: 35 }]}>
+                        Confirm Password
+                    </Text>
+                    <View style={styles.action}>
+                        <FontAwesome // email icon and text field
+                            name="lock"
+                            color="#05375a"
+                            size={20}
+                        />
+                        <TextInput
+                            placeholder="Confirm Password"
+                            secureTextEntry={true}
+                            style={styles.textInput}
+                            value={this.state.confirmpassword}
+                            onChangeText={(val) => this.inputValueUpdate(val, 'confirmpassword')}
+                            autoCapitalize="none"
+                        />
+                        {/* <Feather name="eye-off" color="grey" size={20} /> */}
+                    </View>
+                    <View style={styles.button}>
+                        <TouchableOpacity
+                            
+                            onPress={() => this.storeUser()} 
+                            style={[
+                                styles.signIn,
+                                {
+                                    borderColor: "#05375a",
+                                    borderWidth: 1,
+                                    marginTop: 15,
+                                    backgroundColor: "#05375a",
+                                },
+                            ]}
+                        >
+                            <Text style={[styles.textSign, { color: "#fff" }]}>Sign Up</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity
+                            onPress={() => this.props.navigation.navigate("Log In")}
+                            style={[
+                                styles.signIn,
+                                {
+                                    borderColor: "#05375a",
+                                    borderWidth: 1,
+                                    marginTop: 15,
+                                },
+                            ]}
+                        >
+                            <Image style={styles.icon} source={BackArrow} />
+                        </TouchableOpacity>
+                    </View>
+                </Animatable.View>
+            </View>
+        );
+    }
 }
+
 
 import { StyleSheet } from "react-native";
 
@@ -208,3 +277,5 @@ const styles = StyleSheet.create({
     right: 0,
   },
 });
+
+export default SignUpScreen;
