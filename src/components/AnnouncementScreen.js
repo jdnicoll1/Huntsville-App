73b9838283f7615d2e21a2b8dd8a26238bca9743
Jacob Nicoll <jdnicoll1@gmail.com>
2021-03-09 16:29:50 -0600

    Announcement Screen UI

--- src/components/AnnouncementScreen.js
+++ src/components/AnnouncementScreen.js
@@ -15,21 +15,36 @@ import {
 } from "react-native";
 
 const { width, height } = Dimensions.get("screen");
-import faker from "faker";
+import faker from "faker"; // just for list keys that we will need to assign later
 
-// fake data to fill announcement bubbles for now
-faker.seed(10);
+// set the correct date to display
+var currentdate = new Date();
+var hours = currentdate.getHours();
+var ampm = hours >= 12 ? "pm" : "am";
+hours = hours % 12;
+hours = hours ? hours : 12; // the hour '0' should be '12'
+var minutes = currentdate.getMinutes();
+minutes = minutes < 10 ? "0" + minutes : minutes;
+var datetime =
+  hours +
+  ":" +
+  minutes +
+  ampm +
+  " " +
+  (currentdate.getMonth() + 1) +
+  "/" +
+  currentdate.getDate() +
+  "/" +
+  currentdate.getFullYear();
+
+// PASS INFO HERE
 const DATA = [...Array(30).keys()].map((_, i) => {
   return {
-    key: faker.random.uuid(),
-    image: `https://randomuser.me/api/portraits/${faker.helpers.randomize([
-      "women",
-      "men",
-    ])}/${faker.random.number(60)}.jpg`,
-    name: "School Closed",
-    jobTitle:
+    key: faker.random.uuid(), // each item in a list needs a key - we will need to assign this later
+    announcementTitle: "School Closed",
+    announcementText:
       "This is the announcement text portion, here announcements will be made about school cancellations, road closures, weather advisories, etc",
-    email: "Timestamp",
+    dateTime: datetime,
   };
 });
 
@@ -37,10 +52,21 @@ const SPACING = 20;
 const AVATAR_SIZE = 70;
 // end fake data
 
-export default function AnnouncementScreen({ navigation }) {
+export default function AnnouncementScreen(props) {
+  StatusBar.setBarStyle("dark-content", true); // sets the time and carrier on iphone to black so it shows against the white background
   return (
-    <View style={{ flex: 1, backgroundColor: "#fff", paddingTop: "20%" }}>
-      <Text style={{ textAlign: "center", fontSize: 25 }}>Announcements</Text>
+    <View style={{ flex: 1, backgroundColor: "#fff", paddingTop: "15%" }}>
+      <Text
+        style={{
+          textAlign: "center",
+          fontSize: 30,
+          opacity: 0.8,
+          paddingBottom: "3%",
+        }}
+      >
+        Announcements
+      </Text>
+      <Header {...props} />
       <FlatList
         data={DATA}
         keyExtractor={(item) => item.key}
@@ -63,27 +89,19 @@ export default function AnnouncementScreen({ navigation }) {
                   width: 0,
                   height: 10,
                 },
-                shadowOpacity: 0.2,
+                shadowOpacity: 0.15,
                 shadowRadius: 10,
               }}
             >
-              {/* <Image
-                source={{ uri: item.image }}
-                style={{
-                  width: AVATAR_SIZE,
-                  height: AVATAR_SIZE,
-                  borderRadius: AVATAR_SIZE,
-                }}
-              /> */}
               <View>
                 <Text style={{ fontSize: 28, fontWeight: "700" }}>
-                  {item.name}
+                  {item.announcementTitle}
                 </Text>
                 <Text style={{ fontSize: 18, opacity: 0.7 }}>
-                  {item.jobTitle}
+                  {item.announcementText}
                 </Text>
-                <Text style={{ fontSize: 18, opacity: 0.8, color: "#0099cc" }}>
-                  {item.email}
+                <Text style={{ fontSize: 15, opacity: 0.8, color: "#0099cc" }}>
+                  {item.dateTime}
                 </Text>
               </View>
             </View>
@@ -93,3 +111,45 @@ export default function AnnouncementScreen({ navigation }) {
     </View>
   );
 }
+
+function Header(props) {
+  const menuImage = require("../assets/images/menu.png");
+  const closeImage = require("../assets/images/close_icon.png");
+  // TODO, find a way to change menuImage to closeImage when pressed
+  // const [status, setStatus] = useState(false);
+  // const navigation = useNavigation(); // tp get navigation
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity onPress={() => props.navigation.toggleDrawer()}>
+        <Image style={styles.menu} source={menuImage} />
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    position: "absolute", // menu will not work with this on, might just be a really small target point
+    top: 50,
+    zIndex: 100, //will allow menu png to display above any homescreen photo
+    flexDirection: "row",
+    justifyContent: "flex-end", // would be 'space-between' if you wanted to add a logo on the right
+    width: "30%",
+    alignSelf: "flex-end",
+    height: "10%",
+    paddingHorizontal: 25,
+    paddingTop: 10,
+    // borderWidth: 1,
+  },
+  menu: {
+    width: 28,
+    height: 28,
+    resizeMode: "contain",
+  },
+  closeIcon: {
+    backgroundColor: "white",
+    width: 28,
+    height: 28,
+  },
+});
