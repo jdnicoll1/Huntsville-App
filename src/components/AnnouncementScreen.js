e09e7fbfe808ef4076265dccf15f1fb2ed9847b1
rosspod612 <rosspod612@tamu.edu>
2021-03-25 19:09:36 -0500

    working on announcements screen

--- src/components/AnnouncementScreen.js
+++ src/components/AnnouncementScreen.js
@@ -1,4 +1,4 @@
-import React, { Component, useState } from "react";
+import React, { Component } from "react";
 import {
   StatusBar,
   FlatList,
@@ -12,11 +12,15 @@ import {
   Easing,
   SafeAreaViewBase,
   SafeAreaView,
-  TouchableWithoutFeedback,
+  ActivityIndicator,
 } from "react-native";
 
 const { width, height } = Dimensions.get("screen");
 import faker from "faker"; // just for list keys that we will need to assign later
+import firebase from "../database/firebaseDb";
+import 'firebase/firestore';
+
+
 // set the correct date to display
 var currentdate = new Date();
 var hours = currentdate.getHours();
@@ -36,16 +40,103 @@ var datetime =
   currentdate.getDate() +
   "/" +
   currentdate.getFullYear();
+/*
+function getAnnouncements() {
+    const [loading, setLoading] = useState(true); // Set loading to true on component mount
+    const [announcements, setAnnouncements] = useState([]); // Initial empty array of users
 
+    useEffect(() => {
+        const event = firestore()
+            .collection('announcements')
+            .onSnapshot(() => {
+                useEffect(() => {
+                    const subscriber = firestore()
+                        .collection('announcements')
+                        .onSnapshot(querySnapshot => {
+                            const announcements = [];
+
+                            querySnapshot.forEach(documentSnapshot => {
+                                users.push({
+                                    ...documentSnapshot.data(),
+                                    key: documentSnapshot.id,
+                                });
+                            });
+
+                            setAnnouncements(announcements);
+                            setLoading(false);
+                        });
+
+                    // Unsubscribe from events when no longer in use
+                    return () => event();
+                }, []);
+
+            });
+
+        // Unsubscribe from events when no longer in use
+        return () => event();
+    }, []);
+
+    if (loading) {
+        return <ActivityIndicator />;
+    }
+
+    if (loading) {
+        return <ActivityIndicator />;
+    }
+
+    return (
+        <FlatList
+            data={announcements}
+            renderItem={({ item }) => (
+                <View style={{ height: 50, flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                    <Text>User ID: {item.id}</Text>
+                    <Text>User Name: {item.name}</Text>
+                </View>
+            )}
+        />
+    );
+}*/
+
+function getAnnouncements() {
+    var announcementsArray = [];
+    /*
+    const db = firebase.firestore();
+    const announcementsRef = db.collection('announcements');
+    const snapshot = announcementsRef.where('active', '==', 'true').get();
+    */
+    //return firebase.firestore().collection('announcements').where('active', '==', 'true').get()
+    return firebase.firestore().collection('announcements').orderBy("active", "asc").get()
+        .then((querySnapshot) => {
+            console.log("A");
+            querySnapshot.forEach((doc) => {
+                console.log(dox.id, doc.data());
+            });
+        })
+        .catch((error) => {
+        console.log("Error getting documents: ", error);
+
+        });
+    /*
+    const query = announcementsRef.where('active', '==', 'true').get().then(snapshot => {
+        console.log("A")
+        snapshot.docs.forEach(doc => {
+            console.log("B")
+            announcementsArray.push(doc.data().text());
+            console.log(dox.id, doc.data())
+        })
+    });*/
+    //return announcementsArray;
+} 
 // PASS INFO HERE
-const DATA = [...Array(30).keys()].map((_, i) => {
-  return {
-    key: faker.random.uuid(), // each item in a list needs a key - we will need to assign this later
-    announcementTitle: "School Closed",
-    announcementText:
-      "This is the announcement text portion, here announcements will be made about school cancellations, road closures, weather advisories, etc",
-    dateTime: datetime,
-  };
+const DATA = [...Array(1).keys()].map((_, i) => {
+    const announcementArray = getAnnouncements();
+    return {
+        key: faker.random.uuid(), // each item in a list needs a key - we will need to assign this later
+        announcementTitle: announcementArray[0],
+        announcementText:
+        "This is the announcement text portion, here announcements will be made about school cancellations, road closures, weather advisories, etc",
+        dateTime: datetime,
+    };
 });
 
 const SPACING = 20;
@@ -53,14 +144,13 @@ const AVATAR_SIZE = 70;
 // end fake data
 
 export default function AnnouncementScreen(props) {
-  const [refreshing, setRefreshing] = useState(false);
   StatusBar.setBarStyle("dark-content", true); // sets the time and carrier on iphone to black so it shows against the white background
   return (
-    <View style={{ flex: 1, backgroundColor: "#fff", paddingTop: "15.7%" }}>
+    <View style={{ flex: 1, backgroundColor: "#fff", paddingTop: "15%" }}>
       <Text
         style={{
           textAlign: "center",
-          fontSize: 25,
+          fontSize: 30,
           opacity: 0.8,
           paddingBottom: "3%",
         }}
@@ -77,43 +167,37 @@ export default function AnnouncementScreen(props) {
         }}
         renderItem={({ item, index }) => {
           return (
-            <TouchableWithoutFeedback>
-              <View
-                style={{
-                  flexDirection: "row",
-                  padding: SPACING,
-                  marginBottom: SPACING,
-                  backgroundColor: "rgba(255,255,255,0.8)",
-                  borderRadius: 12,
+            <View
+              style={{
+                flexDirection: "row",
+                padding: SPACING,
+                marginBottom: SPACING,
+                backgroundColor: "rgba(255,255,255,0.8)",
+                borderRadius: 12,
 
-                  shadowColor: "#000",
-                  shadowOffset: {
-                    width: 0,
-                    height: 10,
-                  },
-                  shadowOpacity: 0.15,
-                  shadowRadius: 10,
-                }}
-              >
-                <View>
-                  <Text style={{ fontSize: 28, fontWeight: "700" }}>
-                    {item.announcementTitle}
-                  </Text>
-                  <Text style={{ fontSize: 18, opacity: 0.7 }}>
-                    {item.announcementText}
-                  </Text>
-                  <Text
-                    style={{ fontSize: 15, opacity: 0.8, color: "#0099cc" }}
-                  >
-                    {item.dateTime}
-                  </Text>
-                </View>
+                shadowColor: "#000",
+                shadowOffset: {
+                  width: 0,
+                  height: 10,
+                },
+                shadowOpacity: 0.15,
+                shadowRadius: 10,
+              }}
+            >
+              <View>
+                <Text style={{ fontSize: 28, fontWeight: "700" }}>
+                  {item.announcementTitle}
+                </Text>
+                <Text style={{ fontSize: 18, opacity: 0.7 }}>
+                  {item.announcementText}
+                </Text>
+                <Text style={{ fontSize: 15, opacity: 0.8, color: "#0099cc" }}>
+                  {item.dateTime}
+                </Text>
               </View>
-            </TouchableWithoutFeedback>
+            </View>
           );
         }}
-        refreshing={refreshing}
-        // onRefresh={() => {setMessages}} // here is where we will put the updated info from the backend
       />
     </View>
   );
@@ -138,7 +222,7 @@ function Header(props) {
 const styles = StyleSheet.create({
   container: {
     position: "absolute", // menu will not work with this on, might just be a really small target point
-    top: "6.61%",
+    top: 50,
     zIndex: 100, //will allow menu png to display above any homescreen photo
     flexDirection: "row",
     justifyContent: "flex-end", // would be 'space-between' if you wanted to add a logo on the right
